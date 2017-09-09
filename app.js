6831b45cf14bb9fc7cf52c0bb2e247378a4c7283
Matt <271films@gmail.com>
2017-09-09 19:20:58 +0000

    change to imperial units, add temp

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    request("http://api.openweathermap.org/data/2.5/weather?q=Tampa,us&appid=5063db8217f218b6ce83232444c4e9cd", function(error, response, body){
+    request("http://api.openweathermap.org/data/2.5/weather?q=Tampa,us&units=imperial&appid=5063db8217f218b6ce83232444c4e9cd", function(error, response, body){
         if(!error && response.statusCode == 200) {
-            var weather = JSON.parse(body);
-            res.render("index", {weather: weather});
+            var data = JSON.parse(body);
+            res.render("index", {data: data});
         }
     });
 });
@@ -19,4 +19,4 @@ app.get("/", function(req, res){
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("IRMA is UP");
-})
+});
